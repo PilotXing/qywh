61537e334d3cd27f0be45f3d0f610118769b9a81
PilotXing <xingzihui@gmail.com>
2020-11-27 01:57:40 +0800

    1

---
+++ company_culture_class.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+
+class question(question, choice, answer, familiarity):
+    """
+    docstring
+    """
+
+    def __init__(self, question, choice, answer, familiarity):
+        """
+        docstring
+        """
+        pass
